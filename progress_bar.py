1a8184864c7ed57dbdceb0311c27a46400f727fb
Gabor Szabo <gszab88@gmail.com>
2018-03-31 00:06:05 +0200

    progress bar functions modul for commandline and ipynb

---
+++ progress_bar.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+
+
+def log_progress(sequence, every=None, size=None, name='Items'):
+    from ipywidgets import IntProgress, HTML, VBox
+    from IPython.display import display
+
+    is_iterator = False
+    if size is None:
+        try:
+            size = len(sequence)
+        except TypeError:
+            is_iterator = True
+    if size is not None:
+        if every is None:
+            if size <= 200:
+                every = 1
+            else:
+                every = int(size / 200)     # every 0.5%
+    else:
+        assert every is not None, 'sequence is iterator, set every'
+
+    if is_iterator:
+        progress = IntProgress(min=0, max=1, value=1)
+        progress.bar_style = 'info'
+    else:
+        progress = IntProgress(min=0, max=size, value=0)
+    label = HTML()
+    box = VBox(children=[label, progress])
+    display(box)
+
+    index = 0
+    try:
+        for index, record in enumerate(sequence, 1):
+            if index == 1 or index % every == 0:
+                if is_iterator:
+                    label.value = '{name}: {index} / ?'.format(
+                        name=name,
+                        index=index
+                    )
+                else:
+                    progress.value = index
+                    label.value = u'{name}: {index} / {size}'.format(
+                        name=name,
+                        index=index,
+                        size=size
+                    )
+            yield record
+    except:
+        progress.bar_style = 'danger'
+        raise
+    else:
+        progress.bar_style = 'success'
+        progress.value = index
+        label.value = "{name}: {index}".format(
+            name=name,
+            index=str(index or '?')
+        )
+
+
+def cmd_progress_bar(status):
+    percentage = status*100
+    progBar_stat1 = int(percentage/2)
+    progBar_stat2 = 0
+    if progBar_stat1 > 25:
+        progBar_stat2 = progBar_stat1-25
+        progBar_stat1 = 25
+   
+    sys.stdout.write('\r')
+    sys.stdout.write("[{0: <25} {1}% {2: <25}]".format('='*progBar_stat1, int(percentage), '='*progBar_stat2))
+    sys.stdout.flush()
+
